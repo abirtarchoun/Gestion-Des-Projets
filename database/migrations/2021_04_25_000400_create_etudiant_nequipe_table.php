3d4c12b7bfd4b3cf60d126110bfa045597dc9cee
abirtarchoun <abirtarchoun.8@gmail.com>
2021-04-26 18:10:57 +0100

    update Pivot etudiant_nequipe etudiant_projet

--- database/migrations/2021_04_25_000400_create_etudiant_nequipe_table.php
+++ database/migrations/2021_04_25_000400_create_etudiant_nequipe_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateEtudiantNequipesTable extends Migration
+class CreateEtudiantNequipeTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateEtudiantNequipesTable extends Migration
      */
     public function up()
     {
-        Schema::create('etudiant__nequipes', function (Blueprint $table) {
+        Schema::create('etudiant_nequipe', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedBigInteger('etudiant_id');
             $table->unsignedBigInteger('equipe_id');
@@ -32,6 +32,6 @@ class CreateEtudiantNequipesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('etudiant__nequipes');
+        Schema::dropIfExists('etudiant_nequipe');
     }
 }
